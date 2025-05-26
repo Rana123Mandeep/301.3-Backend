63f87f2f29a4803d8f3a4ec615346aec7791239a
Rana123Mandeep <writetomandeeprana@gmail.com>
2025-05-27 11:02:35 +1200

    update product.html and connect dataase with postgressSQl

--- app.py
+++ app.py
@@ -17,43 +17,79 @@ class Shoes(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    name = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Numeric, nullable=False)
-    gender = db.Column(db.String(10), nullable=False)
-    main_image = db.Column(db.String(50), nullable=False)
-    Thumb_One =  db.Column(db.String(50), nullable=False)
-    Thumb_Two =  db.Column(db.String(50), nullable=False)
-    Thumb_Three =  db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    sizes = db.Column(db.String(100), nullable=False)
+    model = db.Column(db.String(100), nullable=False)
+    style = db.Column(db.String(100), nullable=False)
+    brand= db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(1000), nullable=False)
+    main_image = db.Column(db.String(100), nullable=False)
+    Thumb_One =  db.Column(db.String(100), nullable=False)
+    Thumb_Two =  db.Column(db.String(100), nullable=False)
+    Thumb_Three =  db.Column(db.String(100), nullable=False)
 
-    def __init__(self, name_ = "", price_ = 0, gender_ = "" , main_image_ = "" ,Thumb_one_ = "" , Thumb_Two_ = "", Thumb_Three_ = "" ):
-        self.name = name_
-        self.price = price_
-        self.gender = gender_
-        self.main_image = main_image_
-        self.Thumb_One =Thumb_one_
-        self.Thumb_Two = Thumb_Two_
-        self.Thumb_Three = Thumb_Three_
+    # def __init__(self, name_ = "", price_ = 0, gender_ = "" , main_image_ = "" ,Thumb_one_ = "" , Thumb_Two_ = "", Thumb_Three_ = "" ):
+    #     self.name = name_
+    #     self.price = price_
+    #     self.gender = gender_
+    #     self.main_image = main_image_
+    #     self.Thumb_One =Thumb_one_
+    #     self.Thumb_Two = Thumb_Two_
+    #     self.Thumb_Three = Thumb_Three_
+
+@app.route('/add_product')
+def add_product():
+    temp_title = "Puma RS-X DRIFT PRM"
+    temp_price = 220
+    temp_category = "men"
+    temp_sizes = "7, 8, 9, 10, 11, 12"
+    temp_model = "RS-X DRIFT PRM"
+    temp_style = "Lifestyle Shoes"
+    temp_brand = "Puma"
+    temp_image_url = "https://images.footlocker.com/is/image/FLEU/244205134604_01?wid=500&hei=500&fmt=png-alpha" 
+    temp_main_image = "puma_rsxdrift_main.jpg"
+    temp_Thumb_one = "puma_rsxdrift_thumb1.jpg"
+    temp_Thumb_Two = "puma_rsxdrift_thumb2.jpg"
+    temp_Thumb_Three = "puma_rsxdrift_thumb3.jpg"
+
+    
+    new_shoe = Shoes(
+    title=temp_title,
+    price=temp_price,
+    category=temp_category,
+    sizes=temp_sizes,
+    model=temp_model,
+    style=temp_style,
+    brand=temp_brand,
+    image_url=temp_image_url,
+    main_image=temp_main_image,
+    Thumb_One=temp_Thumb_one,
+    Thumb_Two=temp_Thumb_Two,
+    Thumb_Three=temp_Thumb_Three
+    )
+
+    try:
+        db.session.add(new_shoe)
+        db.session.commit()
+    except:
+        return "Error comitting item"
+    
+    return "Added Item"
+
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/product/<int:shoe_id>', methods=['POST', 'GET'])
-def product(shoe_id):
-    shoe = Shoes.query.get_or_404(shoe_id)  # Fetch the shoe from the database
 
-    # Handle the form submission for POST requests
-    if request.method == "POST":
-        selected_size = request.form.get("size")
-        shipping_option = request.form.get("shippingOption")
-        
-        return jsonify({
-            "message": "POST received",
-            "selected_size": selected_size,
-            "shipping_option": shipping_option
-        })
-
-    # Handle GET request, and render the template
-    return render_template("product.html", shoe=shoe)  # Pass the shoe object to the template
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    print(product_id)
+    shoes = Shoes.query.get(product_id)
+    print(shoes.title)
+    return render_template("product.html", shoe=shoes)
 
      
 
